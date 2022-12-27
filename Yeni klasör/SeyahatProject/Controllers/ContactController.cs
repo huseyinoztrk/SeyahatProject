efca32b1f40422d43d725f89490a9774a20dea4c
Hüseyin <91679832+huseyinoztrk@users.noreply.github.com>
2022-12-27 18:08:20 +0300

    SeyahatProjectt

---
+++ Yeni klasör/SeyahatProject/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeyahatProject.DBContext;
+using SeyahatProject.Models;
+
+namespace SeyahatProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly Context _Context;
+
+        public ContactController(Context context)
+        {
+            _Context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult iletisimGonder(iletisim ilt)
+        {
+            iletisim _ilt = new()
+            {
+                AdSoyad = ilt.AdSoyad,
+                Mail = ilt.Mail,
+                Konu = ilt.Konu,
+                Mesaj = ilt.Mesaj,
+            };
+            _Context.iletisims.Add(_ilt);
+            _Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Yeni klasör/SeyahatProject/Controllers/PhotoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeyahatProject.DBContext;
+
+namespace SeyahatProject.Controllers
+{
+    public class PhotoController : Controller
+    {
+        private readonly Context _Context;
+
+        public PhotoController(Context context)
+        {
+            _Context = context;
+        }
+        public IActionResult Index()
+        {
+            var degerler = _Context.Blogs.ToList();
+            return View(degerler);
+        }
+    }
+}
