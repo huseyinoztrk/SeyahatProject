89fedf563f669aa05dd17f3ee9b2d4f6d27e5308
Hüseyin <91679832+huseyinoztrk@users.noreply.github.com>
2023-01-09 13:58:29 +0300

    Update Program.cs

--- SeyahatProject/Program.cs
+++ SeyahatProject/Program.cs
@@ -1,11 +1,35 @@
 using SeyahatProject.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Localization settings
+builder.Services.AddLocalization(option => { option.ResourcesPath = "Resources"; });
+builder.Services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+
+//Localization Cookie setting
+builder.Services.Configure<RequestLocalizationOptions>(
+    option =>
+    {
+        var languageSupport = new List<CultureInfo>
+        {
+            new CultureInfo("tr"),
+            new CultureInfo("en")
+        };
+
+        option.DefaultRequestCulture = new RequestCulture("tr");
+        option.SupportedCultures = languageSupport;
+        option.SupportedUICultures = languageSupport;
+    }
+    );
+
 builder.Services.AddControllersWithViews();
 
+//Authentication settings
 builder.Services.AddAuthentication(
     CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(option =>
@@ -33,6 +57,15 @@ app.UseRouting();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.UseRequestLocalization();
+
+//var languageSupported = new[] { "tr", "en" };
+//var optionsLocalization = new RequestLocalizationOptions().SetDefaultCulture(languageSupported[0])
+//    .AddSupportedCultures(languageSupported)
+//    .AddSupportedUICultures(languageSupported);
+
+//app.UseRequestLocalization(languageSupported);
 
 app.MapControllerRoute(
     name: "default",
