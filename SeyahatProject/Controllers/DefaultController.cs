49e81f22d538ceda888dde47f675ec50b9644c96
Hüseyin <91679832+huseyinoztrk@users.noreply.github.com>
2023-01-09 13:59:39 +0300

    Update DefaultController.cs

--- SeyahatProject/Controllers/DefaultController.cs
+++ SeyahatProject/Controllers/DefaultController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
 using Newtonsoft.Json;
 using SeyahatProject.DBContext;
 
@@ -7,14 +9,26 @@ namespace SeyahatProject.Controllers
     public class DefaultController : Controller
     {
         private readonly Context _Context;
-
-        public DefaultController(Context context)
+        private readonly IHtmlLocalizer<DefaultController> _localizer;
+        public DefaultController(Context context, IHtmlLocalizer<DefaultController> localizer)
         {
             _Context = context;
+            _localizer = localizer;
+        }
+
+        [HttpPost]
+        public IActionResult MenagerLanguage(string language, string urlReturn)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(language)),
+            new CookieOptions { Expires = DateTimeOffset.Now.AddDays(15)});
+            //return RedirectToAction(nameof(Index));
+            return LocalRedirect(urlReturn);
         }
 
         public IActionResult Index()
         {
+            var testDescController = _localizer["anasayfa"];
+            ViewData["anasayfa"] = testDescController;
             var degerler = _Context.Blogs.Take(8).ToList();
             return View(degerler);
         }
